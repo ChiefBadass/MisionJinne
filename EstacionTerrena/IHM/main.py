dd3748c99029dbfe29daed179c2236c00a2de658
ChiefBadass <carlosdrt900@gmail.com>
2024-05-22 12:42:20 -0600

    se modifico la interfaz

--- EstacionTerrena/IHM/main.py
+++ EstacionTerrena/IHM/main.py
@@ -4,6 +4,7 @@ import pyqtgraph as pg
 from communication import Communication
 from dataBase import data_base
 from PyQt5.QtWidgets import QPushButton
+from PyQt5.QtGui import QIcon, QPixmap
 
 from graphs.grafica_aceleracion import GraficaAceleracion
 from graphs.graph_altitude import graph_altitude
@@ -15,126 +16,156 @@ from graphs.graph_speed import graph_speed
 from graphs.graph_temperature import graph_temperature
 from graphs.graph_time import graph_time
 from graphs.graph_distance import graph_distance
+pg.setConfigOption('background', (236, 236, 236))
+pg.setConfigOption('foreground', (0, 0, 0))
 
-pg.setConfigOption('background', (33, 33, 33))
-pg.setConfigOption('foreground', (197, 198, 199))
-# Interface variables
-app = QtWidgets.QApplication(sys.argv)
-view = pg.GraphicsView()
-Layout = pg.GraphicsLayout()
-view.setCentralItem(Layout)
-view.show()
-view.setWindowTitle('HORUS SPACE LAB - MISION JINNE')
-view.resize(1200, 700)
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
 
-# declare object for serial Communication
-ser = Communication()
-# declare object for storage in CSV
-data_base = data_base()
-# Fonts for text items
-font = QtGui.QFont()
-font.setPixelSize(90)
+        self.setWindowTitle('HORUS SPACE LAB - MISION JINNE')
+        self.setWindowIcon(QIcon('xd.jpg'))  # Añadir el icono aquí
+        self.resize(1200, 700)
 
-# buttons style
-boton_estilo = "background-color:rgb(29, 185, 84);color:rgb(0,0,0);font-size:14px;"
-boton_emergencia_estilo = "background-color:rgb(255, 0, 0);color:rgb(255,255,255);font-size:14px; width: 200px; height: 100px;"
+        # Declarar objetos de comunicación y almacenamiento
+        self.ser = Communication()
+        self.data_base = data_base()
 
-# Button 1
-proxy = QtWidgets.QGraphicsProxyWidget()
-save_button = QtWidgets.QPushButton('Iniciar almacenamiento')
-save_button.setStyleSheet(boton_estilo)
-save_button.clicked.connect(data_base.start)
-proxy.setWidget(save_button)
+        # Crear el widget central
+        central_widget = QtWidgets.QWidget()
+        self.setCentralWidget(central_widget)
 
-# Button 2
-proxy2 = QtWidgets.QGraphicsProxyWidget()
-end_save_button = QtWidgets.QPushButton('Deneter almacenamiento')
-end_save_button.setStyleSheet(boton_estilo)
-end_save_button.clicked.connect(data_base.stop)
-proxy2.setWidget(end_save_button)
+        # Crear un layout vertical para el widget central
+        layout = QtWidgets.QVBoxLayout(central_widget)
+        layout.setContentsMargins(0, 0, 0, 0)
+        layout.setSpacing(0)
 
-# Button Emergency
-proxy3 = QtWidgets.QGraphicsProxyWidget()
-emergency_buttom = QtWidgets.QPushButton('Despliegue de Emergencia')
-emergency_buttom.setStyleSheet(boton_emergencia_estilo)
-emergency_buttom.clicked.connect(ser.paro_emergencia)
-proxy3.setWidget(emergency_buttom)
+        # Crear el widget para el título
+        title_bar = QtWidgets.QWidget()
+        title_bar.setFixedHeight(50)  # Ajusta la altura del título
+        title_bar.setStyleSheet("background-color: #1E1D4F;")  # Color de fondo del título
 
-# Inicializacion de graficas
-grafica_altitud = graph_altitude()
-grafica_aceleraciones = GraficaAceleracion()
-grafica_giroscopio = graph_gyro()
-grafica_presion = graph_pressure()
-grafica_temperatura = graph_temperature()
-grafica_distancia = graph_distance()
-time = graph_time(font=font)
-free_fall = graph_free_fall(font=font)
+        # Crear el label para el texto del título
+        title_label = QtWidgets.QLabel("HORUS SPACE LAB - MISION JINNE")
+        title_label.setAlignment(QtCore.Qt.AlignCenter)
+        title_label.setStyleSheet("""
+            color: white; 
+            font-size: 20px;
+            font-weight: bold;
+        """)
+        
+        # Crear el label para el icono del título
+        icon_label = QtWidgets.QLabel()
+        pixmap = QPixmap('xd.jpg')  # Asegúrate de que 'logo.png' está en el directorio correcto
+        icon_label.setPixmap(pixmap)
+        icon_label.setFixedSize(200, 200)  # Ajustar el tamaño del icono
 
+        # Añadir el label al widget del título
+        title_layout = QtWidgets.QHBoxLayout(title_bar)
+        title_layout.addWidget(icon_label)
+        title_layout.addWidget(title_label)
 
-lb = Layout.addLayout(colspan=21)
-lb.addItem(proxy)
-lb.nextCol()
-lb.addItem(proxy2)
+        # Crear la vista y el layout de pyqtgraph
+        view = pg.GraphicsView()
+        graphics_layout = pg.GraphicsLayout()
+        view.setCentralItem(graphics_layout)
 
-Layout.nextRow()
+        # Añadir widgets al layout
+        layout.addWidget(title_bar)
+        layout.addWidget(view)
 
-l1 = Layout.addLayout(colspan=10, rowspan=2)
-l11 = l1.addLayout(rowspan=1, border=(83, 83, 83))
+        # Inicialización de gráficos y botones
+        self.initialize_graphics_and_buttons(graphics_layout)
 
-# Altitude, speed
-l11.addItem(grafica_altitud)
-l11.addItem(grafica_temperatura)
+        # Configuración del temporizador de actualización
+        self.setup_update_timer()
 
-# l11.addItem(speed)
-l1.nextRow()
+    def initialize_graphics_and_buttons(self, Layout):
+        # Fonts for text items
+        font = QtGui.QFont()
+        font.setPixelSize(90)
 
-# Acceleration, gyro, pressure, temperature
-l12 = l1.addLayout(rowspan=1, border=(83, 83, 83))
-l12.addItem(grafica_presion)
-l12.addItem(grafica_aceleraciones)
+        # Buttons style
+        boton_estilo = "background-color:rgb(29, 185, 84);color:rgb(0,0,0);font-size:14px;"
+        boton_emergencia_estilo = "background-color:rgb(255, 0, 0);color:rgb(255,255,255);font-size:14px; width: 200px; height: 100px;"
 
-# l12.addItem(pressure)
-l1.nextRow()
-l13 = l1.addLayout(rowspan=1, border=(83, 83, 83))
-l13.addItem(grafica_giroscopio)
-l13.addItem(grafica_distancia)
-l1.nextRow()
+    
 
-# Time, battery and free fall graphs
-l2 = l1.addLayout(rowspan=1, border=(83, 83, 83))
-l2.setFixedHeight(100)
-l2.addItem(time)
-l2.nextCol()
-l2.addItem(proxy3)
-l2.nextCol()
-l2.addItem(free_fall)
+        # Button Emergency
+        proxy3 = QtWidgets.QGraphicsProxyWidget()
+        emergency_button = QtWidgets.QPushButton('Despliegue de Emergencia')
+        emergency_button.setStyleSheet(boton_emergencia_estilo)
+        emergency_button.clicked.connect(self.ser.paro_emergencia)
+        proxy3.setWidget(emergency_button)
 
-def update():
-    try:
-        datos = ser.obtener_datos_separados()
+        # Inicialización de gráficos como atributos de la clase
+        self.grafica_altitud = graph_altitude()
+        self.grafica_aceleraciones = GraficaAceleracion()
+        self.grafica_giroscopio = graph_gyro()
+        self.grafica_presion = graph_pressure()
+        self.grafica_temperatura = graph_temperature()
+        self.grafica_distancia = graph_distance()
+        self.time = graph_time(font=font)
+        self.free_fall = graph_free_fall(font=font)
 
-        grafica_temperatura.update(datos['temperatura'])
-        grafica_presion.update(datos['presion'])
-        grafica_altitud.update(datos['altitud'])
-        grafica_aceleraciones.update(datos['aceleracion_x'], datos['aceleracion_y'], datos['aceleracion_z'])
-        grafica_giroscopio.update(datos['gyro_x'], datos['gyro_y'], datos['gyro_z'])
-        grafica_distancia.update(datos['latitud_carga_primaria'], datos['longitud_carga_primaria'], datos['latitud_carga_secundaria'], datos['longitud_carga_secundaria'], datos['distancia'])
+       
 
-        archivo = open('C:/Users/carlo/Documents/python/Mision_Jinne-main/flight_data.csv', 'a')
-        archivo.write(f"{datos}/n")
-        archivo.close()
-    except IndexError:
-        print('starting, please wait a moment')
+        l1 = Layout.addLayout(colspan=10, rowspan=2)
+        l11 = l1.addLayout(rowspan=1, border=(83, 83, 83))
 
+        # Altitude, speed
+        l11.addItem(self.grafica_altitud)
+        l11.addItem(self.grafica_temperatura)
 
-if (ser.isOpen()) or (ser.dummyMode()):
-    timer = pg.QtCore.QTimer()
-    timer.timeout.connect(update)
-    timer.start(100)
-else:
-    print("something is wrong with the update call")
-# Start Qt event loop unless running in interactive mode.
+        l1.nextRow()
+
+        # Acceleration, gyro, pressure, temperature
+        l12 = l1.addLayout(rowspan=1, border=(83, 83, 83))
+        l12.addItem(self.grafica_presion)
+        l12.addItem(self.grafica_aceleraciones)
+
+        l1.nextRow()
+        l13 = l1.addLayout(rowspan=1, border=(83, 83, 83))
+        l13.addItem(self.grafica_giroscopio)
+        l13.addItem(self.grafica_distancia)
+        l1.nextRow()
+
+        # Time, battery and free fall graphs
+        l2 = l1.addLayout(rowspan=1, border=(83, 83, 83))
+        l2.setFixedHeight(100)
+        l2.addItem(self.time)
+        l2.nextCol()
+        l2.addItem(self.free_fall)
+        l2.nextCol()
+        
+        l2.addItem(proxy3)
+
+    def setup_update_timer(self):
+        if self.ser.isOpen() or self.ser.dummyMode():
+            self.timer = pg.QtCore.QTimer()
+            self.timer.timeout.connect(self.update)
+            self.timer.start(100)
+        else:
+            print("something is wrong with the update call")
+
+    def update(self):
+        try:
+            datos = self.ser.obtener_datos_separados()
+
+            self.grafica_temperatura.update(datos['temperatura'])
+            self.grafica_presion.update(datos['presion'])
+            self.grafica_altitud.update(datos['altitud'])
+            self.grafica_aceleraciones.update(datos['aceleracion_x'], datos['aceleracion_y'], datos['aceleracion_z'])
+            self.grafica_giroscopio.update(datos['gyro_x'], datos['gyro_y'], datos['gyro_z'])
+            self.grafica_distancia.update(datos['latitud_carga_primaria'], datos['longitud_carga_primaria'], datos['latitud_carga_secundaria'], datos['longitud_carga_secundaria'], datos['distancia'])
+
+            with open('C:/Users/carlo/Documents/python/Mision_Jinne-main/flight_data.csv', 'a') as archivo:
+                archivo.write(f"{datos}\n")
+        except IndexError:
+            print('starting, please wait a moment')
 
 if __name__ == '__main__':
-    if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
-        QtWidgets.QApplication.instance().exec_()
+    app = QtWidgets.QApplication(sys.argv)
+    main_window = MainWindow()
+    main_window.show()
+    sys.exit(app.exec_())
